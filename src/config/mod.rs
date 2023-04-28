b529d561b2cda0de7492791e06f536bec5ef3d0e
Yannick Peter <yannick.peter@empiriecom.com>
2023-04-28 15:58:31 +0200

    implement get diff for extended commit

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,16 +1,16 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    pub general: GeneralCfg,
+    pub clusters: Vec<Cluster>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(tag = "general")]
 pub struct GeneralCfg {
     #[serde(rename = "time-format")]
     pub time_format: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Stage {
-    pub name: String,
-    pub weight: i8,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -20,7 +20,7 @@ pub struct Cluster {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Config {
-    pub general: GeneralCfg,
-    pub clusters: Vec<Cluster>,
+pub struct Stage {
+    pub name: String,
+    pub weight: i8,
 }
--- src/git/mod.rs
+++ src/git/mod.rs
@@ -1,4 +1,7 @@
-use git2::{Error, Repository, Sort};
+use std::str::from_utf8;
+
+use git2::{Error, Object, ObjectType, Repository, Sort};
+use git2::{Diff, DiffOptions, DiffStatsFormat};
 use chrono::prelude::*;
 use crate::CONFIG;
 
@@ -10,7 +13,7 @@ pub struct ExtendedCommit {
     pub date: String,
 }
 
-impl<'a> ExtendedCommit {
+impl ExtendedCommit {
     fn to_string(self) -> String {
         let str = format!(
             "[ ] {}: {}: {}: {}",
@@ -20,6 +23,14 @@ impl<'a> ExtendedCommit {
             self.summary,
         );
         str
+    }
+
+    pub fn get_commit_diff(self, repo: &Repository) -> String {
+        let diff_obj = get_commit_file_diff(repo, &self.id).unwrap();
+        let stats = diff_obj.stats().unwrap();
+        let buf = stats.to_buf(DiffStatsFormat::FULL, 80).unwrap();
+        let diff = from_utf8(&*buf).unwrap().to_string();
+        diff
     }
 }
 
@@ -47,8 +58,8 @@ pub fn get_commits<'a>(repo: &'a Repository) -> Result<Vec<String>, Error> {
 
         let extd_commit = ExtendedCommit {
             id,
-            summary: commit.summary().unwrap_or("").to_string(),
-            author: commit.author().name().unwrap_or("unknown").to_string(),
+            summary: String::from(commit.summary().unwrap_or("")),
+            author: String::from(commit.author().name().unwrap_or("unknown")),
             date,
         };
 
@@ -64,4 +75,19 @@ fn convert_timestamp(ts: i64) -> String {
     let date = datetime.format(CONFIG.general.time_format.as_str());
     let date_string = date.to_string();
     return date_string;
+}
+
+fn get_commit_file_diff<'a>(repo: &'a Repository, commit_id: &String) -> Result<Diff<'a>, Error> {
+    let mut opts = DiffOptions::new();
+    let tree = tree_to_treeish(repo, &commit_id.as_str()).unwrap().unwrap();
+    repo.diff_tree_to_workdir_with_index(tree.as_tree(), Some(&mut opts))
+}
+
+fn tree_to_treeish<'a>(
+    repo: &'a Repository,
+    arg: &'a str
+) -> Result<Option<Object<'a>>, Error> {
+    let obj = repo.revparse_single(arg)?;
+    let tree = obj.peel(ObjectType::Tree)?;
+    Ok(Some(tree))
 }
